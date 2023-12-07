5af104cb85cad57f964e960839e42b1846158d9a
EdersonIFRS <37879185+EdersonIFRS@users.noreply.github.com>
2023-12-07 17:27:22 -0300

    Add files via upload

---
+++ assets/errorHandler-74847911.js
@@ -0,0 +1,1 @@
+import{s as a,a3 as s,as as n}from"./index-462dc4aa.js";function f(e,r){a.error((r==null?void 0:r.friendlyMsg)||s(e).message||"Unknown"),n(e,r)}export{f as e};
---
+++ assets/useQueryString-4398cfec.js
@@ -0,0 +1,1 @@
+import{aL as e,r as a}from"./index-462dc4aa.js";function o(){const{search:r}=e();return a.useMemo(()=>new URLSearchParams(r),[r])}export{o as u};
